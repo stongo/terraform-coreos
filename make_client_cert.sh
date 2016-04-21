5bea5a595a90bd46a7d70d6aaac23a1f9d9fd361
Nathan LaFreniere <quitlahok@gmail.com>
2016-04-21 12:56:19 -0700

    drop an env file in the client dir

--- make_client_cert.sh
+++ make_client_cert.sh
@@ -7,3 +7,8 @@ terraform output ca_cert > client/ca.pem
 openssl req -new -nodes -keyout client/client.key -out client/client.csr -subj "/C=US/ST=WA/L=Richland/O=andyet/CN=etcd-client"
 openssl x509 -req -days 365 -in client/client.csr -CA client/ca.pem -CAkey client/ca.key -out client/client.pem -set_serial $RANDOM
 rm client/ca.key client/client.csr
+cat << EOF > client/env
+export ETCDCTL_CA_FILE=$PWD/client/ca.pem
+export ETCDCTL_KEY_FILE=$PWD/client/client.key
+export ETCDCTL_CERT_FILE=$PWD/client/client.pem
+EOF
